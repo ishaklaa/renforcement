a7e7497d675bd5452fc45a5ab2cc7c3b11f24392
ishak laaouafi <laaouafi6@example.com>
2026-04-01 09:38:06 +0100

    week-3 ex 1

---
+++ renforcemment/week-3/day-1.php
@@ -0,0 +1,65 @@
+<?php
+
+class Produit
+{
+    private $nom;
+    private $prix;
+    private $stock;
+
+    public function _construct($nom, $prix, $stock)
+    {
+        $this->nom = $nom;
+        $this->prix = $prix;
+        $this->stock = $stock;
+    }
+
+
+    public function getNom()
+    {
+        return $this->nom;
+    }
+
+    public function getPrix()
+    {
+        return $this->prix;
+    }
+
+    public function getStock()
+    {
+        return $this->stock;
+    }
+
+    public function setNom($nom)
+    {
+        $this->nom = $nom;
+    }
+
+    public function setPrix($prix)
+    {
+        if ($prix <= 0) {
+            echo "le prix est negatif ";
+        } else {
+            $this->prix = $prix;
+        }
+    }
+
+    public function setStock($stock)
+    {
+        if ($this->stock <= 0) {
+            echo "le stock est negatif";
+        } else {
+            $this->stock = $stock;
+        }
+    }
+
+    public function afficher()
+    {
+        echo '[' . $this->nom . '] - [' . $this->prix . '] $' . $this->stock;
+    }
+}
+
+$cahier = new Produit();
+$cahier->setNom('diftar');
+$cahier->setPrix(-5);
+$cahier->setStock(10);
+
